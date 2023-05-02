29b0d168ee9afdc02ff07f682eac21cb8671ab82
Volodymyr Bobko <volodymyr-bobko@users.noreply.github.com>
2023-05-02 19:20:09 +0200

    s

--- Tasks/SubmitSigningRequest/task.ts
+++ Tasks/SubmitSigningRequest/task.ts
@@ -1,126 +1,202 @@
 import axios, { AxiosError } from 'axios';
-import * as core from '@actions/core'
+import * as core from '@actions/core';
+import * as coreArtifact from '@actions/artifact';
 import * as polly from 'polly-js'
+import * as fs from 'fs';
+import * as path from 'path';
 import * as moment from 'moment';
+import * as filesize from 'filesize'
 import { SubmitSigningRequestResult } from './DTOs/submit-signing-request-result';
 import { signingRequestStatusCheckdDelays } from './utils';
 import { SignPathUrlBuilder } from './signpath-url-builder';
-import { SigningRequestDto } from './DTOs/signing-request';
+import { SigningRequestDto, SigningRequestStatus } from './DTOs/signing-request';
 
 const MaxWaitingTimeForSigningRequestCompletionMs = 1000 * 60 * 60;
 const MinDelayBetweenSigningRequestStatusChecksMs = 1000 * 60; // start from 1 min
 const MaxDelayBetweenSigningRequestStatusChecksMs = 1000 * 60 * 20; // check at least every 30 minutes
 
 
-export class Task{
+export class Task {
+    urlBuilder: SignPathUrlBuilder;
+
+    constructor () {
+        this.urlBuilder = new SignPathUrlBuilder(this.signPathApiUrl, this.signPathConnectorUrl);
+    }
+
     async run() {
         try {
-            core.info('Execution started...');
-            const connectorUrl = core.getInput('SignPathConnectorUrl', { required: true });
-            const artifactName = core.getInput('ArtifactName', { required: true });
-            const signPathApiBaseUrl = core.getInput('SignPathApiUrl', { required: true });
-            const signPathOrganizationId = core.getInput('OrganizationId', { required: true });
-            const urlBuilder = new SignPathUrlBuilder(signPathApiBaseUrl, connectorUrl);
 
-            core.info('Submitting the signing request to the CI connector...');
+            const signingRequestId = await this.submitSigningRequest();
+            core.setOutput('signingRequestId', signingRequestId);
+            const signingRequest = await this.ensureSigningRequestCompleted(signingRequestId);
+            const signedArtifactFilePath = await this.dowloadTheSigninedArtifact(signingRequest);
+            await this.logArtifactFileStat(signedArtifactFilePath);
+            const artifactClient = coreArtifact.create();
+            core.info('Registering the signed artifact in the artifacts list...');
+            artifactClient.uploadArtifact(`${this.artifactName}-signed`,
+                [path.basename(signedArtifactFilePath)],
+                path.dirname(signedArtifactFilePath));
 
-            const submitRequestPayload = {
-                ciUserToken: core.getInput('CIUserToken', { required: true }),
-                artifactName,
-
-                gitHubApiUrl: process.env.GITHUB_API_URL,
-                gitHubWorkflowRef: process.env.GITHUB_WORKFLOW_REF,
-                gitHubWorkflowSha: process.env.GITHUB_WORKFLOW_SHA,
-                gitHubWorkflowRunId: process.env.GITHUB_RUN_ID,
-                gitHubWorkflowRunAttempt: process.env.GITHUB_RUN_ATTEMPT,
-                gitHubRepository: process.env.GITHUB_REPOSITORY,
-                gitHubToken: core.getInput('GitHubToken', { required: true }),
-
-                signPathOrganizationId,
-                signPathProjectSlug: core.getInput('ProjectSlug', { required: true }),
-                signPathSigningPolicySlug: core.getInput('SigningPolicySlug', { required: true }),
-                signPathArtifactConfigurationSlug: core.getInput('ArtifactConfigurationSlug', { required: true })
-            };
-
-            const response = (await axios
-                .post<SubmitSigningRequestResult>(urlBuilder.buildSubmitSigningRequestUrl(),
-                submitRequestPayload,
-                { responseType: "json" })
-                .catch((e: AxiosError) => {
-                    if(e.response?.data && typeof(e.response.data) === "string") {
-                        throw new Error(e.response.data);
-                    }
-                    throw new Error(e.message);
-                }))
-                .data;
-
-            if (response.error) {
-                throw new Error(response.error);
-            }
-
-            if (response.validationResult && response.validationResult.errors.length > 0) {
-
-                core.startGroup('CI system setup validation errors')
-
-                core.error(`[error]Build artifact \"${artifactName}\" cannot be signed because of continuous integration system setup validation errors:`);
-
-                response.validationResult.errors.forEach(validationError => {
-                    core.error(`[error]${validationError.error}`);
-                    if (validationError.howToFix)
-                    {
-                        core.info(validationError.howToFix);
-                    }
-                });
-
-                core.endGroup()
-
-                throw new Error("CI system vlidation failed.");
-            }
-
-            if (response.signingRequestId) {
-                core.info('SignPath signing request has been successfully submitted.');
-                core.info(`You can view the signing request here: ${response.signingRequestUrl}`);
-                core.setOutput('signingRequestId', response.signingRequestId);
-
-                // check for status update
-                const requestData = await polly.default()
-                  .waitAndRetry(signingRequestStatusCheckdDelays(
-                    MaxWaitingTimeForSigningRequestCompletionMs,
-                    MinDelayBetweenSigningRequestStatusChecksMs,
-                    MaxDelayBetweenSigningRequestStatusChecksMs
-                  ))
-                  .executeForPromise(async () => {
-                    core.info('Checking SignPath signing request status...');
-                    return await axios
-                      .get<SigningRequestDto>(
-                        urlBuilder.buildGetSigningRequestUrl(signPathOrganizationId, response.signingRequestId),
-                       { responseType: "json" })
-                       .then(res => {
-                         if(!res.data.isFinalStatus) {
-                            core.info(`The signing request status is ${res.data.signingRequestStatus}, which is not a final status; after delay, we will check again...`);
-                            throw new Error(`Status ${res.data.signingRequestStatus} is not a final status, we need to check again.`);
-                         }
-                         return res.data;
-                       });
-                  });
-
-                if (!requestData.isFinalStatus) {
-                    const maxWaitingTime = moment.utc(MaxWaitingTimeForSigningRequestCompletionMs).format("hh:mm");
-                    core.error(`We have exceeded the maximum waiting time, which is ${maxWaitingTime}, and the signing request is still not in a final state.`);
-                    core.error(`Signing request status is ${requestData.signingRequestStatus}`);
-                    throw new Error('Incomplete signing request');
-                } else {
-                    core.debug('');
-                }
-
-            }
-            else {
-                throw new Error('Invalid submit signing request result.');
-            }
-
+            core.info('The artifact has been successfully added.');
         }
         catch (err) {
             core.setFailed((err as any).message);
         }
     }
+
+    get signPathConnectorUrl(): string {
+        return core.getInput('SignPathConnectorUrl', { required: true });
+    }
+
+    get artifactName(): string {
+        return core.getInput('ArtifactName', { required: true });
+    }
+
+    get signPathApiUrl(): string {
+        return core.getInput('SignPathApiUrl', { required: true });
+    }
+
+    get organizationId(): string {
+        return core.getInput('OrganizationId', { required: true });
+    }
+
+    get signPathToken(): string {
+        return core.getInput('CIUserToken', { required: true });
+    }
+
+    private async submitSigningRequest (): Promise<string> {
+
+        core.info('Submitting the signing request to SignPath CI connector...');
+
+        // prepare the payload
+        const submitRequestPayload = {
+            ciUserToken: this.signPathToken,
+            artifactName: this.artifactName,
+            gitHubApiUrl: process.env.GITHUB_API_URL,
+            gitHubWorkflowRef: process.env.GITHUB_WORKFLOW_REF,
+            gitHubWorkflowSha: process.env.GITHUB_WORKFLOW_SHA,
+            gitHubWorkflowRunId: process.env.GITHUB_RUN_ID,
+            gitHubWorkflowRunAttempt: process.env.GITHUB_RUN_ATTEMPT,
+            gitHubRepository: process.env.GITHUB_REPOSITORY,
+            gitHubToken: core.getInput('GitHubToken', { required: true }),
+            signPathOrganizationId: this.organizationId,
+            signPathProjectSlug: core.getInput('ProjectSlug', { required: true }),
+            signPathSigningPolicySlug: core.getInput('SigningPolicySlug', { required: true }),
+            signPathArtifactConfigurationSlug: core.getInput('ArtifactConfigurationSlug', { required: true })
+        };
+
+        // call the signPath API to submit the signing request
+        const response = (await axios
+            .post<SubmitSigningRequestResult>(this.urlBuilder.buildSubmitSigningRequestUrl(),
+            submitRequestPayload,
+            { responseType: "json" })
+            .catch((e: AxiosError) => {
+                if(e.response?.data && typeof(e.response.data) === "string") {
+                    throw new Error(e.response.data);
+                }
+                throw new Error(e.message);
+            }))
+            .data;
+
+        if (response.error) {
+            // got error from the connector
+            throw new Error(response.error);
+        }
+
+        if (response.validationResult && response.validationResult.errors.length > 0) {
+
+            // got validation errors from the connector
+            core.startGroup('CI system setup validation errors')
+
+            core.error(`[error]Build artifact \"${this.artifactName}\" cannot be signed because of continuous integration system setup validation errors:`);
+
+            response.validationResult.errors.forEach(validationError => {
+                core.error(`[error]${validationError.error}`);
+                if (validationError.howToFix)
+                {
+                    core.info(validationError.howToFix);
+                }
+            });
+
+            core.endGroup()
+
+            throw new Error("CI system vlidation failed.");
+        }
+
+        core.info(`SignPath signing request has been successfully submitted.`);
+        core.info(`You can view the signing request here: ${response.signingRequestUrl}`);
+
+        return response.signingRequestId;
+    }
+
+    async ensureSigningRequestCompleted(signingrequestId: string): Promise<SigningRequestDto> {
+        // check for status update
+        const requestData = await polly.default()
+            .waitAndRetry(signingRequestStatusCheckdDelays(
+                MaxWaitingTimeForSigningRequestCompletionMs,
+                MinDelayBetweenSigningRequestStatusChecksMs,
+                MaxDelayBetweenSigningRequestStatusChecksMs
+            ))
+            .executeForPromise(async () => {
+                core.info('Checking SignPath signing request status...');
+                return await axios
+                    .get<SigningRequestDto>(
+                        this.urlBuilder.buildGetSigningRequestUrl(this.organizationId, signingrequestId),
+                        { responseType: "json" })
+                    .then(res => {
+                        if(!res.data.isFinalStatus) {
+                            core.info(`The signing request status is ${res.data.signingRequestStatus}, which is not a final status; after delay, we will check again...`);
+                            throw new Error(`Status ${res.data.signingRequestStatus} is not a final status, we need to check again.`);
+                        }
+                        return res.data;
+                    });
+            });
+
+        core.error(`Signing request status is ${requestData.signingRequestStatus}`);
+        if (!requestData.isFinalStatus) {
+            const maxWaitingTime = moment.utc(MaxWaitingTimeForSigningRequestCompletionMs).format("hh:mm");
+            core.error(`We have exceeded the maximum waiting time, which is ${maxWaitingTime}, and the signing request is still not in a final state.`);
+            throw new Error('The signing request is not completed.');
+        } else {
+            if (requestData.signingRequestStatus !== SigningRequestStatus.Completed) {
+                throw new Error('The signing request is not completed.');
+            }
+        }
+
+        return requestData;
+    }
+
+    async dowloadTheSigninedArtifact(signingRequest: SigningRequestDto): Promise<string> {
+        const workingDir = process.env.GITHUB_WORKSPACE as string;
+        const fileName = `${this.artifactName}_signed.zip`;
+        const artifactFileName = `${this.artifactName}_signed`;
+        const targetFilePath = path.join(workingDir, fileName);
+
+        core.info(`The signed artifact is being downloaded from SignPath and will be saved to ${targetFilePath}`);
+
+        const writer = fs.createWriteStream(targetFilePath)
+        const response = await axios.get(signingRequest.SignedArtifactLink, {
+            responseType: 'stream',
+            headers: {
+                Authorization: 'Bearer ' + this.signPathToken
+            }
+        });
+        response.data.pipe(writer);
+        await new Promise((resolve, reject) => {
+            writer.on('finish', resolve)
+            writer.on('error', reject)
+        });
+
+        core.info("The signed artifact has been successfully downloaded from SignPath.");
+        return targetFilePath;
+    }
+
+    async logArtifactFileStat(artifactPath: string) {
+        await fs.stat(artifactPath, (err, stats) => {
+            const size = filesize.partial({base: 2, standard: "jedec"});
+            core.info("File path: " + artifactPath);
+            core.info("File size: " + size(stats.size));
+        });
+    }
+
 }
