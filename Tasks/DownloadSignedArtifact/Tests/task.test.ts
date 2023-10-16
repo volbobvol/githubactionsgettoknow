46fb77d5828091354081cf8bb55f48059edccc3e
Volodymyr Bobko <volodymyr-bobko@users.noreply.github.com>
2023-10-16 14:58:02 +0200

    unit tests

--- Tasks/DownloadSignedArtifact/Tests/task.test.ts
+++ Tasks/DownloadSignedArtifact/Tests/task.test.ts
@@ -9,3 +9,17 @@ const sandbox = sinon.createSandbox();
 afterEach(() => {
     sandbox.restore();
 });
+
+it('test that the task fails if the signing request status is canceled', async () => {
+    sandbox.stub(core, 'getInput').returns(JSON.stringify({
+        signingRequestStatus: "Canceled",
+        signingRequestUiUrl: "Signing request URL"
+    }));
+
+    const setFailedStub = sandbox.stub(core, 'setFailed');
+    const axiosStub = sandbox.stub(axios, 'get');
+    const task = new Task();
+    await task.run();
+    assert.equal(setFailedStub.calledOnce, true);
+    assert.equal(axiosStub.called, false);
+});
